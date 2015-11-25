fe210e90088bb06efdfb3997e28e38cd2093ba5f
Engin Kurutepe <engin@kurutepe.com>
2015-11-25 16:06:01 +0100

    Carthage support

---
+++ Source/OCHamcrest-iOSTests/OCHamcrest_iOSTests.m
@@ -0,0 +1,39 @@
+//
+//  OCHamcrest_iOSTests.m
+//  OCHamcrest-iOSTests
+//
+//  Created by Engin Kurutepe on 25/11/15.
+//  Copyright © 2015 hamcrest.org. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface OCHamcrest_iOSTests : XCTestCase
+
+@end
+
+@implementation OCHamcrest_iOSTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
